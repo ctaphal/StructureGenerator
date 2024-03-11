c86c3d7576c827038879d75fef91fe02ec17447c
Sriya Chinthalapudi <67525176+ctaphal@users.noreply.github.com>
2024-03-11 18:40:53 +0000

    first commit

--- imgGen.py
+++ imgGen.py
@@ -2,24 +2,27 @@ import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw
 
-# Function to generate molecular structure image
+# Function to generate molecular structure image and save it to disk
 def generate_mol_image(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    if mol is not None:
-        drawer = rdMolDraw2D.MolDraw2DCairo(300, 300)  # You can change the size as per your requirement
-        drawer.DrawMolecule(mol)
-        drawer.FinishDrawing()
-        return drawer.GetDrawingText()
-    else:
-        return None
+
+    images_paths = []
+    for i, smile in enumerate(smiles):
+        mol = Chem.MolFromSmiles(smile)
+        if mol is not None:
+            image_path = f"imgs/molecule_{i}.png"
+            Draw.MolToFile(mol, image_path)
+            images_paths.append(image_path)
+        else:
+            images_paths.append(None)
+    return images_paths
 
 # Read CSV file
 csv_file_path = 'odorMolecules.csv'
 data = pd.read_csv(csv_file_path)
 
-# Add a new column for molecular structure images
-data['Mol_Image'] = data['SMILES'].apply(generate_mol_image)
+# Add a new column for image paths
+data['Image_Path'] = generate_mol_image(data['smiles'])
 
 # Save the updated CSV file
-updated_csv_file_path = 'updated_odorMolecules.csv'
+updated_csv_file_path = 'updated2_odorMolecules.csv'
 data.to_csv(updated_csv_file_path, index=False)
